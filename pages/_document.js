425dfa9dd0cb7ff7cb61db2be1054cd3b5a0deef
Maciek Palmowski <m.palmowski@freshpixels.pl>
2023-04-19 18:01:34 +0200

    Kinsta styling

---
+++ pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head />
+            <body className="font-sans leading-normal text-gray-800 bg-kinsta-dark-blue bg relative">
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
